4d8696a68cdc9ed317258757b5692c932b1e3c67
michellss22 <michellss.gui@gmail.com>
2022-12-25 17:52:04 -0300

    Tratando Erros parte 1

---
+++ 004-Tratando_Erros_Consulta.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.facebook.com/'
+# Aqui utilizei o Face Book como exemplo, pode ser substituído por outras url's
+html = urlopen(url)
+bs = BeautifulSoup(html.read(), 'html.parser')
